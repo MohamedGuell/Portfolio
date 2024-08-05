a76399f74819a122f0d2f7448af7efc0b9473c53
Mohamed Guelladress <62598053+MohamedGuell@users.noreply.github.com>
2024-08-05 10:18:44 +0200

    Update projet.tsx

--- src/components/projet.tsx
+++ src/components/projet.tsx
@@ -8,7 +8,7 @@ export function Projets() {
             <div className='flex justify-center'>
             <Tilt>
             <NeonGradientCard className='m-5 transition duration-500 hover:scale-125 hover:opacity-100 hover:mx-10'>
-                <a href="..src\components\Chess\index.html" id="carte1" className=" no-underline font-[bold] text-[black] flex justify-center items-center shadow-[rgba(50,50,93,0.25)_0px_50px_100px_-20px,rgba(0,0,0,0.3)_0px_30px_60px_-30px,rgba(10,37,64,0.35)_0px_-2px_6px_0px_inset] bg-[url(./assets/chessImg.png)] opacity-[80%] w-[250px] h-[400px] transition-all duration-[0.8s] ease-[ease-out] m-2.5 rounded-[10px]  "></a>
+                <a href="src\components\Chess\index.html" id="carte1" className=" no-underline font-[bold] text-[black] flex justify-center items-center shadow-[rgba(50,50,93,0.25)_0px_50px_100px_-20px,rgba(0,0,0,0.3)_0px_30px_60px_-30px,rgba(10,37,64,0.35)_0px_-2px_6px_0px_inset] bg-[url(./assets/chessImg.png)] opacity-[80%] w-[250px] h-[400px] transition-all duration-[0.8s] ease-[ease-out] m-2.5 rounded-[10px]  "></a>
                 </NeonGradientCard>
             </Tilt>
             </div>
@@ -16,7 +16,7 @@ export function Projets() {
                 <Tilt>
                     
                 <NeonGradientCard className='m-5 transition duration-500 hover:scale-125 hover:opacity-100 hover:mx-10'>
-                    <a href=".src\components\Beps\mainPage.html" id="carte2" className="no-underline font-[bold] text-[black] flex justify-center items-center shadow-[rgba(50,50,93,0.25)_0px_50px_100px_-20px,rgba(0,0,0,0.3)_0px_30px_60px_-30px,rgba(10,37,64,0.35)_0px_-2px_6px_0px_inset] bg-[url(./assets/beps.png)] opacity-[80%] w-[250px] h-[400px] transition-all duration-[0.8s] ease-[ease-out] m-2.5 rounded-[10px] "></a>
+                    <a href="Portfolio\src\components\Beps\beps.html" id="carte2" className="no-underline font-[bold] text-[black] flex justify-center items-center shadow-[rgba(50,50,93,0.25)_0px_50px_100px_-20px,rgba(0,0,0,0.3)_0px_30px_60px_-30px,rgba(10,37,64,0.35)_0px_-2px_6px_0px_inset] bg-[url(./assets/beps.png)] opacity-[80%] w-[250px] h-[400px] transition-all duration-[0.8s] ease-[ease-out] m-2.5 rounded-[10px] "></a>
                     </NeonGradientCard>
                 </Tilt>
             </div>
